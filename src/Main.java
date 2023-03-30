3b7d81ed1341c87a0bbfdd5095dfb0df3db763a2
Telb5 <elbarqtarik@gmail.com>
2023-03-29 20:26:35 -0500

    the color feature have been added

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,11 @@ public class Main {
             audi.color="black";
             audi.model="A3";
             audi.power="5000cc";
+
+            color col= new color();
+            col.colorLights="True";
+            col.colorTyre="false";
+            col.colorWindows="True";
+
         }
     }
